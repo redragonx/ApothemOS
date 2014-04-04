698aa11e4bad155359db7224342845860e9a9752
rabbitfighter <rabbitfighter81@gmail.com>
2014-04-04 16:28:26 -0600

    cLeaned up code, added comments, and specified standards in file info sections.

--- os/source/gpio.s
+++ os/source/gpio.s
@@ -1,124 +1,132 @@
 /******************************************************************************
-*  FileName:gpio.s
+*  FileName: gpio.s
 *  Authors: Stephen Chavez & Joshua Michael Waggoner
 *  Date: Mar 26, 2014
-*  Target: ARMv6 - Application Binary Interface (ABI) Compliant
+*  Target: ARMv6 - Embedded Application Binary Interface (EABI) Compliant
 *
 *  Description: A file that holds information
-*  pertaining to access and manipulation of
-*  GPIO pins in a reusable manner.
+*               pertaining to access and manipulation of
+*               GPIO pins in a reusable manner.
 *  
-*******************************************************************************/
+******************************************************************************/
 
-/****************Explaination of r0, r1 designations****************************
-*r0 will be  for designating the pin we wish to target (0-53)
-*r1 will be  for designating the function of the pin we wish to target (0-7)
+/********************Explaination of r0, r1 designations***********************
+* r0 will be  for designating the pin we wish to target (0-53)
+* r1 will be  for designating the function of the pin we wish to target (0-7)
 *******************************************************************************
-
-
-/*-------------------*/
-/*-----Function------*/
-/*-------------------*/
-
-
 
 /*msg to assembler to make the label GetGpioAddress accessible to all files.*/
 .globl GetGpioAddress 
 
-/* NEW
-* GetGpioAddress returns the base addr of the GPIO region as a physical address
-* in register r0.
+/*
+* GetGpioAddress returns the base addr of the GPIO region as a 
+* physical address in register r0.
 * C++ Signature: void* GetGpioAddress()
 */
 
-GetGpioAddress:		/*Label*/
+GetGpioAddress:			/*Label*/
 	ldr r0, =0x20200000 	/*Load GPIO controller access address in r0*/ 
 	mov pc,lr		/*Move contents of link register into pc*/
 
+/*****************************************************************************/
 
-
-/******************************************************************************/
-		
-
-/*-------------------*/
-/*-----Function------*/
-/*-------------------*/
-
-/*SetGpioFunction sets the function of the GPIO register addressed by r0 to the
-* low 3 bits of r1.*/
-
+/*
+* SetGpioFunction sets the function of the GPIO register addressed 
+* by r0 to the low 3 bits of r1.
+*/
 
 /*msg to assembler to make the label SetGPIOFunction accessible to all files*/
 .globl SetGpioFunction
 
-
-/* NEW
+/* 
 * GetGpioAddress returns the base address of the GPIO region as a physical 
 * address in register r0.
 * C++ Signature: void* GetGpioAddress()
 */
 SetGpioFunction:	/*Label*/
+
    pinNum  .req r0
    pinFunc .req r1
-      cmp pinNum,#53   /*checks if value in r0 is less than 53: Will Skip */
+
+      cmp pinNum,#53    /*checks if value in r0 is less than 53*/
       cmpls pinFunc,#7	/*checks if value in r0 is less than 7*/
-      movhi pc,lr   /*Executes if value stored in cspr is less than #53*/
+      movhi pc,lr       /*Moves lr into pc if value in cspr is less than #53*/
 
       push {lr}		/*Puts lr on the top of the stack*/
-      mov r2,pinNum     /*Move pinNum into R2*/
-      .unreq pinNum
-      pinNum .req r2    /*Alias pinNum to r2*/
-      bl GetGpioAddress       /*Loads GPIO address into r0*/
-      gpioAddr .req r0
 
-	/*This simple loop code compares the pin number to 9. If it is higher 		* than 9, it subtracts 10 from the pin number, and adds 4 to the GPIO 		* Controller address then runs the check again.*/
+/*
+* The next three statements just makes pinNum an alias for r2 instead of r0,
+* and brings it's value with it. 
+*/
+      mov r2,pinNum           /*Move pinNum into R2*/
+      .unreq pinNum	      /*Unalias r2*/
+      pinNum .req r2          /*Alias pinNum to r2*/
+
+      bl GetGpioAddress       /*Loads GPIO address into r0*/
+      gpioAddr .req r0	      /*Alias ro as gpioAddr*/
+
+/*
+* This simple loop code compares the pin number to 9. If it is higher 
+* than 9, it subtracts 10 from the pin number, and adds 4 to the GPIO 
+* Controller address then runs the check again. This is how we find the 
+* GPIO controller address we are looking for.
+*/
       functionLoop$:
-         cmp pinNum,#9 /*compare pin number to 9*/
-         subhi pinNum,#10/*sub 10 from pin number*/
-         addhi gpioAddr,#4 /*adds 4 to the GPIO Controller address*/
+         cmp pinNum,#9 		/*Compare pin number to 9*/
+         subhi pinNum,#10	/*Sub 10 from pin number*/
+         addhi gpioAddr,#4 	/*Adds 4 to the GPIO Controller address*/
          bhi functionLoop$
 
-      add pinNum, pinNum,lsl #1 /*multiplication by 3 in disguise*/
-      lsl pinFunc,pinNum	/*shift  r1 (function code) by the amt in r2*/
-      mask .req r3
-      mov mask, #7            /* r3 = 111 in binary */
+      add pinNum, pinNum,lsl #1 /*Multiplication by 3 in disguise*/
+      lsl pinFunc,pinNum	/*Shift pinFunc by value in pinNum*/
+      mask .req r3		/*Alias r3 as mask*/
+      mov mask, #7              /*r3 = 111 in binary*/
 
-      lsl mask, pinNum /* r3 = 11100..00 where the 111 is in the same position
-			  * as the function in r1 */
-      .unreq pinNum
-      mvn mask, mask  /* r3 = 11..1100011..11 where the 000 is in the same 				 * poisiont as the function in r1 */
+      lsl mask, pinNum          /*
+				* r3 (mask) = 11100..00 where the 111 is in 
+				* the same position as the function in 
+				* r1 (pinFunc).
+				*/
+     
+      .unreq pinNum		/*Unalias pinNum*/
+
+      mvn mask, mask  		/*
+				* r3 = 11..1100011..11 where the 000 is in 
+				* the same poisiont as the function in r1 
+				*/
         
-      oldFunc .req r2 /*Alias oldFunc to r2*/
-      ldr oldFunc,[gpioAddr]
-      and oldFunc, mask
+      oldFunc .req r2 		/*Alias oldFunc to r2. It's the old PinNum*/
+      ldr oldFunc,[gpioAddr]    /*Load what's in gpioAddr into oldFunc*/
+      and oldFunc, mask		/*and r3 and r2*/
 
-      orr pinFunc, oldFunc
-      .unreq oldFunc
+      orr pinFunc, oldFunc	/*orr r1 and r2*/
+      .unreq oldFunc		/*Unaliass oldFunc*/
 	
 
-      str pinFunc,[gpioAddr]	/*Store what's in the address in r0 in r1*/
-      .unreq pinFunc
-      .unreq gpioAddr
-      pop {pc}		/*Pop the stack to restore registers*/
+      str pinFunc,[gpioAddr]	/*Store pinFunc in [gpioAddr] for return*/
+      .unreq pinFunc		/*Customary Unaliasing*/
+      .unreq gpioAddr		/*...*/
+      pop {pc}			/*Pop the stack to restore registers*/
 
-/******************************************************************************/
-	
-/*-------------------*/
-/*-----Function------*/
-/*-------------------*/
+/*****************************************************************************/
 
-/* SetGpio sets the GPIO pin addressed by register r0 high if r1 != 0 and low
-*  otherwise. */
+/* 
+* SetGpio sets the GPIO pin addressed by register r0 high if r1 != 0 and low
+* otherwise. 
+*/
 
+/*msg to assembler to make the label SetGPIOFunction accessible to all files*/
 .globl SetGpio
+
 SetGpio:
    pinNum .req r0	/*Alias pinNum for r0 */
-   pinVal .req r1  /*Alias piVal  for r1 */
+   pinVal .req r1       /*Alias piVal  for r1 */
 
       cmp pinNum,#53	/*Compare pinNum to #52 I.E. See if we have a valid #*/
       movhi pc,lr	/*Move link register into program counter*/
-      push {lr}	/*Push link register on stack*/
+      push {lr}	        /*Push link register on stack*/
       mov r2,pinNum	/*Move pinNumber to r2*/
+
    .unreq pinNum	/*Unalias pinNum*/
    pinNum .req r2	/*Alias pinNum to r2*/
 
@@ -126,15 +134,20 @@ SetGpio:
 
    gpioAddr .req r0	/*Alias gpioAddr for r0*/
 
-      pinBank .req r3		/*Alias for pinBank*/
+      pinBank .req r3	      /*Alias for pinBank*/
       lsr pinBank,pinNum,#5   /*lsl pin# by 5 -> store in pinBank. div by 32*/
-      lsl pinBank,#2		/*Multiply this by 4*/
+      lsl pinBank,#2	      /*Multiply this by 4*/
 
-      /****************Note on the above two lines of code:******************/
-      /*You may wonder if we could just shift it right by 3 places, as we 		went right then left. This won't work , as some of the answer may 	 	 have been rounded when we did ÷ 32 which may not be if we just ÷ 8.*/
-      /**********************************************************************/
+      /****************Note on the above two lines of code:******************
+      
+	You may wonder if we could just shift it right by 3 places, as we 		went right then left. This won't work , as some of the answer may 	 	 have been rounded when we did ÷ 32 which may not be if we just ÷ 8.
+       
+      ***********************************************************************/
 
-      /*This means if we add 28 we get the address for turning the pin on, 		and if we add 40 we get the address for turning the pin off.*/
+     /*
+      * This means if we add 28 we get the address for turning the pin on,
+      * and if we add 40 we get the address for turning the pin off.
+      */
       add gpioAddr,pinBank	/*Add pin bank value to gpio addr*/
 
       .unreq pinBank		/*Unalias pinbank*/
@@ -142,18 +155,23 @@ SetGpio:
       and pinNum,#31		/*and pinNum with 11111(base2)*/
       setBit .req r3		/*Alias r3 as setBit*/
       mov setBit,#1		/*Move 1 into setBit*/
-      lsl setBit,pinNum	/*lsl setBit by pinNum*/
-      .unreq pinNum		/*We don't need pin num anymore, so unalias it*/
+      lsl setBit,pinNum	        /*lsl setBit by pinNum*/
 
-      teq pinVal,#0		/*Test if inVal is equal to 0*/
+      .unreq pinNum		/*We don't need pinNum anymore, so unalias it*/
 
-      .unreq pinVal
-      streq setBit,[gpioAddr,#40] /*If teq = true  turn pin on*/
-      strne setBit,[gpioAddr,#28] /*If teq = false turn pin off*/
-      .unreq setBit		    /*Unalias setBit to be proper*/
-      .unreq gpioAddr
-      pop {pc}/*return by popping the pc, which sets it to the value that we 			 *stored when we pushed the link register.*/			
+      teq pinVal,#0		/*Test if pinVal is equal to 0*/
 
-/******************************************************************************/
+      .unreq pinVal		/*Unalias pinVal to be polite*/
 
+      streq setBit,[gpioAddr,#40]   /*If teq = true  turn pin on*/
+      strne setBit,[gpioAddr,#28]   /*If teq = false turn pin off*/
 
+      .unreq setBit		    /*Unalias setBit*/
+      .unreq gpioAddr		    /*Unalias gpioAddr*/
+
+      pop {pc}		 /*
+			 * return by popping the pc, which sets it to the 
+			 * value that we stored when we pushed the link 			 * register.
+			 */			
+
+/*****************************************************************************/
--- os/source/systemTimer.s
+++ os/source/systemTimer.s
@@ -6,44 +6,55 @@
 *  Target: ARMv6 - Application Binary Interface (ABI) Compliant
 *
 *  Description: systemTime.s contains the code that interacts with the system 
-*  timer.
+*               timer.
 *  
 *******************************************************************************/
 
-/*
-* The system timer runs at 1MHz, and just counts always. Thus we can deduce
-* timings by measuring the difference between two readings.
-*/
+ /*
+ * The system timer runs at 1MHz, and just counts always. Thus we can deduce
+ * timings by measuring the difference between two readings.
+ */
 
-/*
-* GetSystemTimerBase returns the base address of the System Timer region as a
-* physical address in register r0.
-* C++ Signature: void* GetSystemTimerBase()
-*/
+ /*
+ * GetSystemTimerBase returns the base address of the System Timer region as a
+ * physical address in register r0.
+ * C++ Signature: void* GetSystemTimerBase()
+ */
+
 .globl GetSystemTimerBase /*Make this a global function*/
+
 GetSystemTimerBase: 
-	ldr r0,=0x20003000 /*Register used to control and clear timer channel */
-	mov pc,lr /*Move the link register into program counter to return*/
 
-/*
-* GetTimeStamp gets the current timestamp of the system timer, and returns it
-* in registers r0 and r1, with r1 being the most significant 32 bits.
-* C++ Signature: u64 GetTimeStamp()
-*/
+   ldr r0,=0x20003000 /*Register used to control and clear timer channel*/
+   mov pc,lr /*Move the link register into program counter to return*/
+
+/*****************************************************************************/
+
+ /*
+ * GetTimeStamp gets the current timestamp of the system timer, and returns it
+ * in registers r0 and r1, with r1 being the most significant 32 bits.
+ * C++ Signature: u64 GetTimeStamp()
+ */
+
 .globl GetTimeStamp	/*Make this a global function*/
-GetTimeStamp:
-	push {lr} /*Push link register to be able to restore registers*/
-	bl GetSystemTimerBase /*Get the timer's base adress*/
-	ldrd r0,r1,[r0,#4] /*0x20003004 A counter that increments at 1MHz.*/
-	pop {pc} /*Pop the program counter and restore registers*/
 
-/*
-* Wait waits at least a specified number of microseconds before returning.
-* The duration to wait is given in r0.
-* C++ Signature: void Wait(u32 delayInMicroSeconds)
-*/
+GetTimeStamp:
+
+   push {lr} /*Push link register to be able to restore registers*/
+   bl GetSystemTimerBase /*Get the timer's base adress*/
+   ldrd r0,r1,[r0,#4] /*0x20003004 A counter that increments at 1MHz.*/
+   pop {pc} /*Pop the program counter and restore registers*/
+
+ /*
+ * Wait waits at least a specified number of microseconds before returning.
+ * The duration to wait is given in r0.
+ * C++ Signature: void Wait(u32 delayInMicroSeconds)
+ */
+
 .globl Wait	/*Make this a global function*/
+
 Wait:
+
 	delay .req r2	/*Alias r2 as "delay"*/
 	mov delay,r0    /*Move delay into r2 because we know r1,r0 are used*/
 	push {lr}	/*Push link register because we will be braching*/
@@ -51,15 +62,20 @@ Wait:
 	start .req r3	/*Alias r3 as start*/
 	mov start,r0	/*Move lower four bytes to "start"*/
 
-	/*Next we need to compute the difference between the current counter 		 *value and the reading we just took, and then keep doing so until the 		 *gap between them is at least the size of delay.*/
+	 /*
+	 * Next we need to compute the difference between the current counter 		 * value and the reading we just took, and then keep doing so until the 	 * gap between them is at least the size of delay.
+         */
+
 	loop$:
-		bl GetTimeStamp		/*Get the timestamp in r0, r1*/
-		elapsed .req r1		/*Alias r1 as "elapsed"*/
-		sub elapsed,r0,start    /*Subtract elapsed time from start time*/
-		cmp elapsed,delay	/*Compare delay to elapsed*/
-		.unreq elapsed		/*Unalias elapsed*/
-		bls loop$		/*If elapsed is less than delay, loop*/
+	   bl GetTimeStamp	      /*Get the timestamp in r0, r1*/
+	   elapsed .req r1	      /*Alias r1 as "elapsed"*/
+	   sub elapsed,r0,start       /*Start time - elapsed time*/
+	   cmp elapsed,delay	      /*Compare delay to elapsed*/
+	   .unreq elapsed	      /*Unalias elapsed*/
+	      bls loop$		      /*If elapsed is less than delay, loop*/
 		
 	.unreq delay	/*Unalias delay*/
 	.unreq start	/*Unalias start*/
 	pop {pc}	/*Return by popping pc*/
+
+/*****************************************************************************/
